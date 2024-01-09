cc720c543df49654660ade58415357cb2612fc39
Pury-01 <puryokumu@gmail.com>
2024-01-09 13:06:48 +0300

    function that replaces an element in a list at a specific position without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    my_list1 = list(my_list)
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list1
+
+    else:
+        my_list1[idx] = element
+        return my_list1
