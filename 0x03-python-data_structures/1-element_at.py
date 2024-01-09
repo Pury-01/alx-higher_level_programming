d9d97528043d109a7e6b47053d108c9b487cb1b5
Pury-01 <puryokumu@gmail.com>
2024-01-09 11:13:42 +0300

    function that retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
-        for i in range(0, len(my_list), 1):
-            return my_list[idx]
+        return my_list[idx]
