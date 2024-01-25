4d3789d6428835aad2fa9fbc7404d50096df899f
Pury-01 <puryokumu@gmail.com>
2024-01-25 04:34:38 +0300

    print square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -54,36 +54,28 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """Setter method for the position attribute.
-
-        Args:
-        value (tuple): Position value to set.
-
-        Raises:
-        TypeError: If the value is not a tuple of 2 positive integers.
-        """
-        if not isinstance(value, tuple) or len(value) != 2
-        or not all(isinstance(num, int) for num in value)
-        or any(num < 0 for num in value):
+        if (not isinstance(value, tuple) or len(value) != 2
+                or not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Calculate and return the area of the square."""
-        return self.__size ** 2
+        """Return the current are of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square using '#' characters based on size and position."""
+        """Prints the square with the # character."""
         if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            print("")
+            return
+        [print("") for _ in range(0, self.__position[1])]
+        for _ in range(0, self.__size):
+            [print("#", end="") for _ in range(self.__position[0])]
+            [print("#", end="") for _ in range(0, self.size)]
+            print("")
 
     def __str__(self):
-        """String representation of the Square instance."""
         result = ""
         if self.__size == 0:
             result += "\n"
