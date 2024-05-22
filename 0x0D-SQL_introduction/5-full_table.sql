c23a35b33d7514fc29b6c2fe9e215c58cda445a9
Pury-01 <puryokumu@gmail.com>
2024-05-22 19:40:21 +0300

    prints full description of the table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- prints the full description of the table first_table
+-- from database hbtn_0c_0 in mysql server
+-- database name to be passed as an argument of mysql
+
+SHOW CREATE TABLE first_table;
