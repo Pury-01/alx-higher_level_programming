8e3c57c1c077636100dcdde503d2cfc9bd24e8dc
Pury-01 <puryokumu@gmail.com>
2024-05-22 21:16:33 +0300

    creates the mysql server user, with all privileges and doesn't fail if user already exists

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- user has all privileges
 
 CREATE USER IF NOT EXISTS 'user_0d_01'@'localhost' IDENTIFIIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON "." TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
