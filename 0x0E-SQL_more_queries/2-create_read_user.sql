3a6446ee5d7e9030d9e447e62b16b64e1cbee1f2
Pury-01 <puryokumu@gmail.com>
2024-05-22 23:22:04 +0300

    script that creates the database hbtn_0d_2 and the user user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- password set to user_0d_2_pwd
 
 CREATE DATABASE IIF NoT EXISTS hbtn_0d_2;
-CREATE USER IF NoT ExISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd;
+CREATE USER IF NoT ExISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
