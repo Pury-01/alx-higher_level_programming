c4117b58333a7b48360ca8f9bf9434d0e7578fde
Pury-01 <puryokumu@gmail.com>
2024-05-22 20:17:22 +0300

    script that lists all records of the second table by order of the score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the second_table of hbtn_0c_0 database
+-- results display both score and the name
+-- records ordered by score
+
+SELECT score, name FROM second_table ORDER BY score DESC
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- creates a table second_table in hbtn_0c_0 database
+
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
--- script create maximum of 4 records
 INSERT INTRO 'second_table' ('id', 'name', 'score') VALUES (1, "John", 10);
 INSERT INTRO 'second_table' ('id', 'name', 'score') VALUES (2, "Alex", 3);
 INSERT INTRO 'second_table' ('id', 'name', 'score') VALUES (3, "Bob", 14);
