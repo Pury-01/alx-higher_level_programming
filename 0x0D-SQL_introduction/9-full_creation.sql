1a2b2684dc876ab4b53d3e04d407169454c064f4
Pury-01 <puryokumu@gmail.com>
2024-05-22 20:11:34 +0300

    creates a second table in  mysql server and add multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creates a table second_table in hbtn_0c_0 database
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT):
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 -- script create maximum of 4 records
 INSERT INTRO 'second_table' ('id', 'name', 'score') VALUES (1, "John", 10);
