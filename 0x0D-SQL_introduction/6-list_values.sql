862db4e5a0ef86c899f9d57ac707f830b606da04
Pury-01 <puryokumu@gmail.com>
2024-05-22 19:45:13 +0300

    lists all rows of the table, all fields are printed

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- lists all rows of the table first_table
+-- from database hbtn_0c_0 in mysql server
+-- All fields are printed
+-- database name will be passed as an argument
+
+SELECT * FROM first_table;
