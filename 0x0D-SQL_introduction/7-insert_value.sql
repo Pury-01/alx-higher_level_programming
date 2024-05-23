ad42fcb8f278cc21e8b7afe6d6c41883e868c346
Pury-01 <puryokumu@gmail.com>
2024-05-23 15:52:41 +0300

    inserts a new row in a table n the mysql server

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- inserts a new row in the table first_table
 -- from database hbtn_0c_0 in mysql server
 
-INSERT INTO 'first_table' ('id','name') VALUES (89, 'Best School');
+INSERT INTO 'first_table' ('id','name') VALUES (89, "Best School");
