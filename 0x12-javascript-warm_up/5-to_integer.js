a5381d93850571d4155290ca2dd2209d9149c915
Pury-01 <puryokumu@gmail.com>
2024-06-17 15:45:27 +0300

    converts argument into number and prints it

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// converts argument to integer and if it does it prints it
+
+const args = process.argv;
+
+// convert first argument to int
+const myNumber = parseInt(args[2]);
+
+// check if argument is a number
+if (isNaN(myNumber)) {
+  console.log('Not a number');
+} else {
+  console.log(myNumber);
+}
