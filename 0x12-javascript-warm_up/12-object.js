30b30751320f31ce158c1275d2e114e13b9d5a65
Pury-01 <puryokumu@gmail.com>
2024-06-18 10:26:57 +0300

    script that modifies property of a constant object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// replaces value 12 with 89
+
+const myObject = {
+  type: 'object',
+  vaue: 12
+};
+
+console.log(myObject);
+
+// modify the value of myObject
+myObject.value = 89;
+
+console.log(myObject);
