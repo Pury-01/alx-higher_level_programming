ce86280116013294d34a6fef8d0f41a3c097b8f8
Pury-01 <puryokumu@gmail.com>
2024-06-25 00:33:50 +0300

    script that takes in an argument and displays all values of states table where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""takes argument and displays all values in the states table
+where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+
+def states():
+    """take command line arguments."""
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    """connect to MySQL server"""
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+            )
+    """cursor object to query mysql"""
+    cursor = db.cursor()
+
+    """execute query with given input"""
+    query = (
+            "SELECT * FROM states WHERE name = %s"
+            "ORDER BY states.id ASC"
+            )
+    cursor.execute(query, (state_name_searched,))
+
+    """fetch all the results"""
+    results = cursor.fetchall()
+
+    """print results"""
+    for row in results:
+        print(row)
+
+    """close cursor and database connection"""
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    states()
