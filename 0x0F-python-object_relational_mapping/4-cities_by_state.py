e16f552048e487d64183105f643c76db97584452
Pury-01 <puryokumu@gmail.com>
2024-06-27 13:51:28 +0300

    script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""lists all cities from database hbtn_0e_4_usa"""
+
+import MySQLdb
+import sys
+
+
+def cities():
+    """get command-line arguments"""
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    """connect to MySQL server"""
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+            )
+    """cursor object for sql query"""
+    cursor = db.cursor()
+
+    """sql query execution"""
+    query = "SELECT * FROM cities ORDER BY cities.id ASC"
+    cursor.execute(query)
+
+    """fetch all results"""
+    results = cursor.fetchall()
+
+    for result in results:
+        print(result)
+
+    """close cursor and database connection"""
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    cities()
