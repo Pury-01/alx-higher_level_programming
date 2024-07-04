3c65fa23622e6b09f6c72019201f5393d1547cb8
Pury-01 <puryokumu@gmail.com>
2024-07-04 23:23:37 +0300

    script that sends a DELETE request to the URL passed as the first argument and displays the body of the response

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends DELETE request to the URL passed as first argument and displays the body of the response
+# sends DELETE request to the URL passed as first argument
 curl -s -X DELETE "$1"; echo ""
-
