03649baf72af54ca97053a1c23027cb60560e0d4
Pury-01 <puryokumu@gmail.com>
2024-08-07 21:21:44 +0300

    JavaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,7 +1,14 @@
 // updates the text color of the <header> element to red when user clicks on the tag DIV#red_header using JQuery API
 
-$(document).ready(function () {
-  $('#red_header').click(function () {
-    $('header').css('color', '#FF0000');
+(function (callback) {
+  const script = document.createElement('script');
+  script.src = 'https://code.jquery.com/jquery-3.2.1.min.js';
+  script.onload = callback;
+  document.head.appendChild(script);
+})(function () {
+  $(document).ready(function () {
+    $('#red_header').on('click', function () {
+      $('header').css('color', '#FF0000');
+    });
   });
 });
